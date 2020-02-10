c2c3ccf86f485a64e959645bb12e73a87211a660
felixf3600 <felixf3600@gmail.com>
2020-02-10 14:22:32 -0800

    changed to fetch large photo

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${btn.value}.json?api-key=5Fuweavf9NF1wpoTCAyGX4XZ7DMCtztz`
     }).done(function(data) {
-      console.log(data);
       topStories = populate(data);
       displayStories(topStories);
       // .fail(function() {
@@ -20,7 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
       abstract: data[counter].abstract,
       byLine: data[counter].byline,
       url: data[counter].url,
-      photo: data[counter].multimedia[4].url
+      photo: data[counter].multimedia[0].url,
+      caption: data[counter].multimedia[0].caption
     };
     return news;
   }
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const topArticles = [];
     for (
       let counter = 0;
-      counter < 12 && counter <= data.results.length;
+      counter < 12 && counter < data.results.length;
       counter++
     ) {
       topArticles[counter] = getArticle(counter, data.results);
@@ -36,23 +36,47 @@ document.addEventListener("DOMContentLoaded", function() {
     return topArticles;
   }
 
-  function displayStories(storiesArray, counter) {
+  function displayStories(storiesArray) {
     const articlesArea = document.getElementById("news");
+    let counter = 1;
     articlesArea.innerHTML = " ";
     storiesArray.forEach(article => {
       const span = document.createElement("span");
       const aTag = document.createElement("a");
       const image = document.createElement("img");
       image.innerHTML = createImage(article);
-      span.innerHTML = createSpan(article);
-      aTag.innerHTML = createATag(counter, aTag, span, image, article);
+      createSpan(span, article);
+      createATag(counter, aTag, span, image, article);
       articlesArea.appendChild(aTag);
+      counter++;
     });
+    console.log(articlesArea);
   }
 
-  function createATag(article) {}
+  function createImage(article) {
+    return `src=${article.photo} alt=${article.caption}`;
+  }
 
-  function createSpan(article) {}
+  function createSpan(span, article) {
+    const byLineSpan = document.createElement("span");
+    const titleSpan = document.createElement("span");
+    const abstractSpan = document.createElement("span");
+    byLineSpan.setAttribute("class", "byline");
+    titleSpan.setAttribute("class", "title");
+    abstractSpan.setAttribute("class", "abstract");
+    byLineSpan.innerText = article.byLine;
+    titleSpan.innerText = article.title;
+    abstractSpan.innerText = article.abstract;
+    span.appendChild(titleSpan);
+    span.appendChild(byLineSpan);
+    span.appendChild(abstractSpan);
+  }
 
-  function createImage(counter, aTag, span, image, article) {}
+  function createATag(counter, aTag, span, image, article) {
+    aTag.setAttribute("href", article.url);
+    aTag.setAttribute("class", "article");
+    aTag.setAttribute("id", `article${counter}`);
+    aTag.appendChild(image);
+    aTag.appendChild(span);
+  }
 }); // end of doc
